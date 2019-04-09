c804dad3e4bc6167afd5e3be405f35b904a645a6
Miles Marillo <mjm03@hampshire.edu>
2019-04-09 02:23:14 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -13,8 +13,11 @@ def first_challenge
     }
   }
 
-  #your code here
-
+  contacts.eachdo |name, data|
+    if name == "Freddie Mercury"
+  data.eachdo  |key, value|
+    if key == :favorite_icecream_flavors
+    value.delete_if{|flavor| flavor = "strawberry"}
 
   #remember to return your newly altered contacts hash!
   contacts
